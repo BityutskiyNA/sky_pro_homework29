cf39c1b3e478ed859adbd6b6622031283da22f25
BityutskiyNA <BityutskiyNA@gmail.com>
2022-12-20 16:26:38 +0300

    done

--- sky_pro_homework29/urls.py
+++ sky_pro_homework29/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from user.views import LocationViewSet
+
+router = routers.SimpleRouter()
+router.register('location', LocationViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('api-auth/', include('rest_framework.urls')),
+                  path('ad/', include('ads.urls')),
+                  path('cat/', include('category.urls')),
+                  path('user/', include('user.urls')),
+              ]
+
+urlpatterns += router.urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
